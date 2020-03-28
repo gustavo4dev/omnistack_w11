6f253fce84b3c4bd8f5464845e8665ea11433b72
Gustavo <girardigustavo.03@gmail.com>
2020-03-28 17:01:33 -0300

    Aplicação mobile

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,10 +6,12 @@ module.exports = {
 
     const [count] = await connection('incidents').count();
 
+    console.log(count);
+
     const incidents = await connection('incidents')
       .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-      .limit(6)
-      .offset((page - 1) * 6)
+      //.limit(5)
+      //.offset((page - 1) * 5)
       .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
 
     res.header('X-Total-Count', count['count(*)']);
